123d2477ec53d9e65690c6bb257fe6f9cebb0b0c
Ryan Martino <ryanmartino@Ryans-MacBook-Pro.local>
2017-01-06 11:31:08 -0800

    hello world

--- Destroy/ViewController.swift
+++ Destroy/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("ehllo wprld")
+        print("hello wprld")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
